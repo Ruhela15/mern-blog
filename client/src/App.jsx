6813114838a0b9036c5dc42365d60b8c012d3193
Priyanshu <pr129858@gmail.com>
2025-07-27 17:40:38 +0530

    CREATE PAGES AND ROUTE

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,25 @@
 import   React from 'react'
+import { BrowserRouter as Router, Route, Switch, BrowserRouter } from 'react-router-dom';
+import Home from './pages/Home';
+import Project from './pages/Project';
+import Dashboard from './pages/Dashboard';
+import About from './pages/About';
+import Signin from './pages/Signin';
+import Signup from './pages/Signup';
 
+import { Routes, Route } from 'react-router-dom';
 const App = () => {
   return (
-    <div> 
-      <h1 className='bg-red-500 text-white p-4'>Hello world!</h1>
-      <p>This is a simple React application.</p>
-      <p>Welcome to the blog app!</p>
-      <p>Enjoy exploring the features.</p>
-    </div>
+   <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/project" element={<Project />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/signin" element={<Signin />} />
+      <Route path="/signup" element={<Signup />} />
+    </Routes>
+   </BrowserRouter>
   )
 }
 
